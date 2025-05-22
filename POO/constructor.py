9ed4bb064d0d1cf93ae37d7887ae587abb5817a2
Victor Carmona <victorcarmona9035.com>
2025-05-22 23:04:52 +0200

    Constructor en python

---
+++ POO/constructor.py
@@ -0,0 +1,21 @@
+#Defincion de una clase 
+class Persona:
+    
+    #Constructor
+    def __init__(self, nombre, apellido):
+        #Creamos los atributos de la clase
+        self.nombre = nombre
+        self.apellido = apellido
+        
+    def mostrar_persona(self):
+        print(f'''Persona:
+              Nombre:{self.nombre}
+              Apellido:{self.apellido}''')
+        
+
+#Cracion de un objeto
+if __name__ == '__main__':
+    print('*** ----------------------- Creacion de Persona 1 ----------------***')
+    #Creamos un objeto
+    persona1 = Persona('Juan', 'Pérez') #Crea un objeto en memoria
+    persona1.mostrar_persona() #Llamamos al metodo mostrar_persona
