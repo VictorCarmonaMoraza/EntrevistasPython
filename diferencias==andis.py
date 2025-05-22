5fbabd67792c006afc63aab04dc6d9a38f8a4c51
Victor Carmona <victorcarmona9035.com>
2025-05-22 13:17:17 +0200

    diferencias entre == y is en python

---
+++ diferencias==andis.py
@@ -0,0 +1,12 @@
+a =[1,2,3]
+b =[1,2,3]
+
+print(a == b) # True: Tienenel mismo contenido
+
+
+print(a is b) # False: Son objetos diferentes en memoria
+
+#casos
+
+c=a
+print(c is a) # True: Son el mismo objeto en memoria
