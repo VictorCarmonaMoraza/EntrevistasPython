c76c575c9de1cb9a9fa4b797dcaa138c056bc5ab
Victor Carmona <victorcarmona9035.com>
2025-05-22 17:47:29 +0200

    range en python

---
+++ function_range.py
@@ -0,0 +1,12 @@
+
+#Este es el caso más común cuando simplemente quieres repetir algo un número específico de veces.
+# Genera números del 0 al 4 (5 números en total)
+for i in range(5):
+    print(f'{i}--------->Hola{i}')
+
+
+print('------------------')
+#Este es el caso más común cuando simplemente quieres repetir algo un número específico de veces.
+# Genera números del 0 al 4 (Generara numeros del 5 al 8, exclyendo el 8)
+for i in range(5, 8):
+    print(f'{i}--------->Hola{i}')
